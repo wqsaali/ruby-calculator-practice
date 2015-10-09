ab5e66a35c1855329f012acc424e9040341324fb
Ammad Ahmed <ammad.ahmed@pikessoft.com>
2015-10-09 13:24:38 +0500

    Object Created

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@
 
 class Calculator 
 
+
 #declaring Constructor 
   def initialize(n1=0,n2=0)
     @num_1, @num_2 = n1,n2
@@ -12,3 +13,7 @@ class Calculator
 end
 
 
+# Main Calculator Working 
+# Step 1 --> Objecting Calculator
+
+cal = new Calculator
